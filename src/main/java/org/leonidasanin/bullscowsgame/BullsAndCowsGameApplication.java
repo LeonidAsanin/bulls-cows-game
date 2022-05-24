8c50dbde75b804c27e4be4c885c9c9f84b4a2efa
LennardJones <l.asanin@mail.ru>
2022-05-24 19:03:03 +0400

    Initial commit

---
+++ src/main/java/org/leonidasanin/bullscowsgame/BullsAndCowsGameApplication.java
@@ -0,0 +1,13 @@
+package org.leonidasanin.bullscowsgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BullsAndCowsGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BullsAndCowsGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/leonidasanin/bullscowsgame/BullsAndCowsGameApplicationTests.java
@@ -0,0 +1,13 @@
+package org.leonidasanin.bullscowsgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BullsAndCowsGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
